d5ab2dd37cea935bf16563cb6ee7564d2169f3b3
RAM GOLHAR <alliswell01213@gmail.com>
2026-09-06 11:40:04 +0530

    Add default values to AMI and instance type variables

--- variable.tf
+++ variable.tf
@@ -1,9 +1,11 @@
 variable "ami_id" {
-description = "AMI ID for EC2"
-type        = string
+  description = "AMI ID for EC2"
+  type        = string
+  default     = "ami-0b6d9d3d33ba97d99"
 }
 
 variable "instance_type" {
-description = "EC2 instance type"
-type        = string
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
