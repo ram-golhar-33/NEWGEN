89f372382cfbc0d27d7d822937cba483b531708b
RAM GOLHAR <alliswell01213@gmail.com>
2026-09-06 12:09:31 +0530

    Add VPC and subnet data sources with outputs
    Added data sources for VPC and subnet, and outputs for VPC ID, CIDR block, and subnet ID.

--- main.tf
+++ main.tf
@@ -1,4 +1,38 @@
-resource "aws_instance" "my_ec2" {
-ami           = var.ami_id
-instance_type = var.instance_type
+# VPC
+
+data "aws_vpc" "my_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["MY-VPC"]
+  }
+}
+
+
+# Subnet
+
+data "aws_subnet" "my_subnet" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.my_vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["Public-Subnet"]
+  }
+}
+
+
+# Outputs
+
+output "vpc_id" {
+  value = data.aws_vpc.my_vpc.id
+}
+
+output "vpc_cidr" {
+  value = data.aws_vpc.my_vpc.cidr_block
+}
+
+output "subnet_id" {
+  value = data.aws_subnet.my_subnet.id
 }
