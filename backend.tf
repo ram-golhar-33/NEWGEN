201f95db67f0fa436521e0115046188f0e058f0c
RAM GOLHAR <alliswell01213@gmail.com>
2026-09-12 11:59:34 +0530

    Update S3 bucket name for Terraform backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ram-terraform-state-2026"
+    bucket         = "ram-terraform-state-2026-001"
     key            = "terraform/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
