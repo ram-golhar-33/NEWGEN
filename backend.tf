4f297b84ccf16efa467a0a1e64d4c7b679f3169a
RAM GOLHAR <alliswell01213@gmail.com>
2026-09-12 13:15:09 +0530

    Comment out terraform backend configuration

--- backend.tf
+++ backend.tf
@@ -1,4 +1,4 @@
-terraform {
+/* terraform {
   backend "s3" {
     bucket       = "ram-terraform-state-2026-001"
     key          = "terraform/terraform.tfstate"
@@ -6,4 +6,4 @@ terraform {
     encrypt      = true
     use_lockfile = true
   }
-}
+}*/
