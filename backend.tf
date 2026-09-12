007628d28ec5bb915b0de8b7545e3ef5dfb5fa22
RAM GOLHAR <alliswell01213@gmail.com>
2026-09-12 16:40:23 +0530

    Refactor backend.tf for cleaner configuration
    Removed unnecessary comments and adjusted formatting.

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
-/* terraform {
+terraform {
   backend "s3" {
-    bucket       = "ram-terraform-state-2026-001"
-    key          = "terraform/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket = "ram-terraform-state-2026-001"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
-}*/
+}
