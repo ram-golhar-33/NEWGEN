89fe921346d49861c3147001c423b2d02d01ae08
RAM GOLHAR <alliswell01213@gmail.com>
2026-09-12 12:01:28 +0530

    Add use_lockfile option to S3 backend configuration

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ram-terraform-state-2026-001"
-    key            = "terraform/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
+    bucket       = "ram-terraform-state-2026-001"
+    key          = "terraform/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
